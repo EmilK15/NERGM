4915a678d5fd2c1b99b382d389e88d5c3b53d2ed
emil kais <emil.kais94@gmail.com>
2020-01-07 22:44:11 -0500

    Reformatted Dashboard to use React/Apollo hooks again.

--- client/components/presentation/Dashboard.js
+++ client/components/presentation/Dashboard.js
@@ -1,70 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Home } from './';
 import { Loading } from './responses';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 import { GET_ME } from '../graphql/queries';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { get_user, user_error } from '../../redux/actions/userActions';
 import { Topbar } from './topbar';
 
-class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dropDownSelect: '',
-        }
+const dropDownRenderSwitch = navigation => {
+    switch(navigation) {
+        case 'Home':
+            return <Home />;
+        case 'Analytics':
+            return <div>Analytics</div>;
+        case 'My Account':
+            return <div>My Account</div>;
+        default:
+            return <Home />;
     }
+}
 
-    dropDownRenderSwitch(navigation) {
-        switch(navigation) {
-            case 'Home':
-                return <Home />;
-            case 'Analytics':
-                return <div>Analytics</div>;
-            case 'My Account':
-                return <div>My Account</div>;
-            default:
-                return <Home />;
-        }
+const Dashboard = () => {
+    const topbar_navigation = useSelector(state => state.navigation.topbar_navigation);
+    const { loading, error, data } = useQuery(GET_ME);
+    const dispatch = useDispatch();
+
+    if(loading)
+        return <Loading />;
+    if(error) {
+        dispatch(user_error(true));
+        return <Redirect to="/"/>;
     }
-
-    render() {
-        const { topbar_navigation } = this.props;
-        return (
-            <Query query={GET_ME}>
-                {({loading,error,data}) => {
-                    if(loading)
-                        return <Loading />;
-                    if(error){
-                        this.props.user_error(true);
-                        return <Redirect to="/" />;
-                    }
-                    if(data) {
-                        this.props.get_user(data.getMe);
-                        return <div className="dashboard-container">
-                            <Topbar />
-                            { this.dropDownRenderSwitch(topbar_navigation) }
-                        </div>                        
-                    }
-                }}
-            </Query>
-        )
+    if(data) {
+        dispatch(get_user(data.getMe));
+        return <div className="dashboard-container">
+            <Topbar />
+            { dropDownRenderSwitch(topbar_navigation) }
+        </div>   
     }
 };
 
-const mapStateToProps = state => ({
-    topbar_navigation: state.navigation.topbar_navigation
-});
-
-const dispatchToProps = dispatch => {
-    return {
-        user_error: (bool) => dispatch(user_error(bool)),
-        get_user: (user) => dispatch(get_user(user))
-    }
-};
-
-export default connect (
-    mapStateToProps,
-    dispatchToProps,
-)(Dashboard);
+export default Dashboard;
